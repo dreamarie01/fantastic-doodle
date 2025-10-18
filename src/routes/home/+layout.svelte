fb246ebd7a734e4431226b0a1e85f49e26ee0396
dreamarie01 <167934680+dreamarie01@users.noreply.github.com>
2025-10-18 15:33:33 -0600

    added shadcn components

---
+++ src/routes/home/+layout.svelte
@@ -0,0 +1,19 @@
+<script>
+	import '../../app.css';
+	import favicon from '$lib/assets/favicon.svg';
+    import Button from '$lib/components/ui/button/button.svelte';
+    
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<div class="headWrapper flex">
+    <Button href="/" variant="link">Landing</Button>
+    <Button href="/home" variant="link">Home</Button>
+</div>
+
+{@render children?.()}
